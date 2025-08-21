bed1055e8cbd39b6344386e50dfc6dbaba51a6b7
Scott Weitzenkamp <scott_weitzenkamp@yahoo.com>
2025-08-21 03:29:50 -0500

    Initial version from ChatGPT, with 3 beeps added at end.

---
+++ beep.sh
@@ -0,0 +1,10 @@
+for i in {1..30}
+do
+    echo beep
+    echo -ne '\a'  # Terminal bell character
+    sleep 10
+done
+
+echo -ne '\a'  # Terminal bell character
+echo -ne '\a'  # Terminal bell character
+echo -ne '\a'  # Terminal bell character
